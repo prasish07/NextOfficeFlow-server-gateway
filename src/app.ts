9ef4c7d050ddc634076ceecbcac1c19c93079e44
prasish07 <prasishshrestha0099@gmail.com>
2024-05-03 22:11:08 +0545

    Create gateway for microservice api

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import gateway from "fast-gateway";
+import path from "path";
+import dotenv from "dotenv";
+
+const absolutePath = path.join(__dirname, "../.env");
+dotenv.config({ path: absolutePath });
+
+const gatewayPort = process.env.PORT || 5000;
+
+const server = gateway({
+	routes: [
+		{
+			prefix: "/api/v1/announcement",
+			target: "http://localhost:5001",
+		},
+		{
+			prefix: "/api/v1",
+			target: "http://localhost:5002",
+		},
+	],
+});
+
+server.start(gatewayPort as number).then((server) => {
+	console.log(`Gateway server is running on port ${gatewayPort}`);
+});
